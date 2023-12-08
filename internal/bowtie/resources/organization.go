0d465077ceec308ec75a2f484614077649024f3f
Tyler Langlois <tjl@byu.net>
2023-12-08 13:53:54 -0700

    Implement organization resource

---
+++ internal/bowtie/resources/organization.go
@@ -0,0 +1,145 @@
+package resources
+
+import (
+	"context"
+	"time"
+
+	"github.com/bowtieworks/terraform-provider-bowtie/internal/bowtie/client"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ resource.Resource = &organizationResource{}
+var _ resource.ResourceWithImportState = &organizationResource{}
+
+type organizationResource struct {
+	client *client.Client
+}
+
+type organizationResourceModel struct {
+	ID          types.String `tfsdk:"id"`
+	LastUpdated types.String `tfsdk:"last_updated"`
+	Name        types.String `tfsdk:"name"`
+	Domain      types.String `tfsdk:"domain"`
+}
+
+func NewOrganizationResource() resource.Resource {
+	return &organizationResource{}
+}
+
+func (org *organizationResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_organization"
+}
+
+func (org *organizationResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: `
+Manage organization information.
+`,
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Internal resource ID.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"last_updated": schema.StringAttribute{
+				MarkdownDescription: "The last time this object was change by Terraform. This field is _not part of the Bowtie API_ but rather additional provider metadata.",
+				Computed:            true,
+			},
+			"name": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "The human readable name of the organization.",
+			},
+			"domain": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "Domain to associate with this organization.",
+			},
+		},
+	}
+}
+
+func (org *organizationResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*client.Client)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Incorrect provider data",
+			"The provider data was not appropriate and failed to resolve as *client.Client",
+		)
+	}
+
+	org.client = client
+}
+
+func (org *organizationResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	resp.Diagnostics.AddError(
+		"Organization creation is not supported.",
+		"Please instead use an import block or import command if you would like to manage the existing organization.",
+	)
+}
+
+func (org *organizationResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var state organizationResourceModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	org_response, err := org.client.GetOrganization()
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Failed retrieving organization information.",
+			err.Error(),
+		)
+		return
+	}
+
+	state.Name = types.StringValue(org_response.Name)
+	state.Domain = types.StringValue(org_response.Domain)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
+
+func (org *organizationResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan organizationResourceModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	err := org.client.UpsertOrganization(
+		plan.Name.ValueString(),
+		plan.Domain.ValueString(),
+	)
+
+	if err != nil {
+		resp.Diagnostics.AddError("Failed updating organization", err.Error())
+		return
+	}
+
+	plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
+}
+
+func (org *organizationResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	resp.Diagnostics.AddError(
+		"Organization destruction is not supported.",
+		"Please instead remove the resource from your terraform state as Bowtie organizations cannot be removed.",
+	)
+}
+
+func (org *organizationResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
---
+++ internal/bowtie/test/organization_test.go
@@ -0,0 +1,98 @@
+package test
+
+import (
+	"context"
+	"os"
+	"strings"
+	"testing"
+	"text/template"
+
+	"github.com/bowtieworks/terraform-provider-bowtie/internal/bowtie/client"
+	"github.com/bowtieworks/terraform-provider-bowtie/internal/bowtie/provider"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+const (
+	resourceName = "bowtie_organization.org"
+	orgName      = "Test Organization"
+	orgDomain    = "Different DNS Block List name"
+)
+
+func TestAccOrganizationResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: provider.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Import testing. Note that organizations cannot be
+			// created or destroyed, so we rely purely on import to create
+			// the initial resource.
+			{
+				Config:       getOrganizationConfig(resourceName, orgName, orgDomain),
+				ResourceName: resourceName,
+				ImportState:  true,
+				// We can’t control what Id the API comes up with for the
+				// organization ID, so derive it dynamically:
+				ImportStateIdFunc: getOrgId(),
+				// The last_updated attribute does not exist in the HashiCups
+				// API, therefore there is no value for it during import.
+				ImportStateVerifyIgnore: []string{"last_updated"},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "name"),
+					resource.TestCheckResourceAttrSet(resourceName, "domain"),
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttrSet(resourceName, "last_updated"),
+				),
+			},
+		},
+	})
+}
+
+func getOrganizationConfig(resource string, name string, domain string) string {
+	funcMap := template.FuncMap{
+		"notNil": func(val any) bool {
+			return val != nil
+		},
+	}
+
+	tmpl, err := template.New("").Funcs(funcMap).ParseGlob("testdata/*.tmpl")
+	if err != nil {
+		return ""
+	}
+
+	var output *strings.Builder = &strings.Builder{}
+	err = tmpl.ExecuteTemplate(output, "organization.tmpl", map[string]interface{}{
+		"provider":      provider.ProviderConfig,
+		"resource_type": strings.Split(resource, ".")[0],
+		"resource":      strings.Split(resource, ".")[1],
+		"name":          name,
+		"domain":        domain,
+	})
+
+	if err != nil {
+		panic("Failed to render template")
+	}
+
+	return output.String()
+}
+
+func getOrgId() resource.ImportStateIdFunc {
+	return func(state *terraform.State) (string, error) {
+		ctx := context.Background()
+
+		username := os.Getenv("BOWTIE_USERNAME")
+		password := os.Getenv("BOWTIE_PASSWORD")
+
+		client, err := client.NewClient(ctx, "http://localhost:3000", username, password, false)
+
+		if err != nil {
+			return "", err
+		}
+
+		org, err := client.GetOrganization()
+		if err != nil {
+			return "", err
+		}
+
+		return org.ID, nil
+	}
+}
